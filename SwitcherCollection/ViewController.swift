6f369eab81ff67517727681d9498c215c2b299d2
TBXark <vfanx@qq.com>
2015-10-25 13:52:08 +0800

    Initial Commit

---
+++ SwitcherCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitcherCollection
+//
+//  Created by Tbxark on 15/10/25.
+//  Copyright © 2015年 TBXark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
